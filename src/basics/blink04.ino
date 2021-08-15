46a9a366bfd06a9fd0b2d6d909df3d203d63485b
Clivern <hello@clivern.com>
2021-08-16 01:25:43 +0200

    update

--- src/basics/blink04.ino
+++ src/basics/blink04.ino
@@ -2,7 +2,7 @@
 // Use of this source code is governed by the MIT
 // license that can be found in the LICENSE file.
 
-// Hardware Used (To build two circuits)
+// Hardware Used
 // - PC with Arduino IDE
 // - UNO R3 with Cable 1 PCS
 // - Red Led 5 PCS
